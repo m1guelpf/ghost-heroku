7ea10b88f0f90c03c01053730c76e8bf26858ef4
jsyang <jsyang.ca@gmail.com>
2019-10-10 12:15:57 +0100

    Attempt a fix on ghost-heroku due to outdated npm modules

--- content/adapters/storage/ghost-imgur/index.js
+++ content/adapters/storage/ghost-imgur/index.js
@@ -1,47 +1,44 @@
-'use strict'
+// jscs:disable requireMultipleVarDecl
 
-var util = require('util');
-var BaseStorage = require('../../../core/server/storage/base');
-var imgur = require('imgur');
-var Promise = require('bluebird');
+'use strict';
 
-function ghostImgur(config) {
-  this.config = config || {};
-  BaseStorage.call(this);
+var Promise = require('bluebird'),
+    BaseStorage = require('ghost-storage-base'),
+    imgur = require('imgur');
+
+class ImgurStorage extends BaseStorage {
+  constructor() {
+    super();
+  }
+
+  save(image) {
+    // TODO: save delete url
+    return imgur.uploadFile(image.path)
+        .then(function(json) {
+          return json.data.link;
+        })
+        .catch(function(err) {
+          return Promise.reject(err.message);
+        });
+  }
+
+  exists(fileName, targetDir) {
+    // TODO: check file status
+    return Promise.resolve(true);
+  }
+
+  serve() {
+    return function(req, res, next) {
+      next();
+    };
+  }
+
+  delete() {
+    return Promise.reject('not implemented');
+  }
+
+  read(options) {
+  }
 }
 
-util.inherits(ghostImgur, BaseStorage);
-
-ghostImgur.prototype.save = function(file) {
-  // TODO: save delete url
-  return new Promise(function(resolve, reject) {
-    imgur.uploadFile(file.path)
-      .then(function(json) {
-        if(!json || !json.data || !json.data.link) return reject();
-        resolve(json.data.link);
-      })
-      .catch(reject);
-  });
-}
-
-ghostImgur.prototype.serve = function() {
-  return function(req, res, next) {
-    next();
-  };
-}
-
-ghostImgur.prototype.exists = function(filename) {                        
-  return new Promise(function(resolve) {                                  
-    // TODO: check file status                                            
-    resolve(true);                                                        
-  });                                                                     
-}                                                                         
-                                                                          
-ghostImgur.prototype.delete = function(fileName, targetDir) {             
-  // TODO: delete image by url                                            
-  return new Promise(function(resolve) {                                  
-    resolve(false);                                                       
-  });                                                                     
-};
-
-module.exports = ghostImgur;
+module.exports = ImgurStorage;
